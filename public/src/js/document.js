2c1a698de4f378843e3b58ee9a9a309c60b4c470
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-02-09 09:28:47 -0500

    Update document.js
     - Added proper message parsing strucutre in document.js

--- public/src/js/document.js
+++ public/src/js/document.js
@@ -24,12 +24,19 @@ let username = user.email;
 // Database tester code
 document.getElementById('ask').onclick = () => {
 
+  // Retreive the current user reference in the realtime database
   let user_dbRef = db.ref().child("users").child(username).child("post");
+
+  // Get the title of the document contenteditable div
+  let title = document.getElementById("doc-title").innerText;
+  // Get the body (main text body) contenteditable div of the document
+  let body = document.getElementById("doc-body").innerText;
 
   // Post/write/set the data to be a json object
   user_dbRef.set({
 
-    message: "Response sent"
+    title: title,
+    body: body
 
   });
 
