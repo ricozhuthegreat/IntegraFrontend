a7debc330424fda2be7c11239c6fd7cb8948cec3
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-02-09 09:20:48 -0500

    Updated Firebase Dependencies

---
+++ public/src/js/document.js
@@ -0,0 +1,43 @@
+
+// Attach an onclick evetn listener arrow function for firebase signout (via logout button)
+document.getElementById('logout').onclick = () => {
+
+  //
+  firebase.auth().signOut().then(function() {
+    // Sign-out successful, redirect back to the main page (index.html)
+    window.location = "index.html";
+  }, function(error) {
+    // Log any errors
+    console.log("Error: " + error);
+  });
+
+}
+
+// The global firebase database object
+let db = firebase.database();
+
+// Get the currently singed in user
+let user = firebase.auth().currentUser;
+
+let username = "ricozhuthegreat";
+
+// Database tester code
+document.getElementById('poststuff').onclick = () => {
+
+  let user_dbRef = db.ref().child("users").child(username).child("post");
+
+  // Post/write/set the data to be a json object
+  user_dbRef.set({
+
+    message: "Response sent"
+
+  });
+
+}
+
+// Attach a listner for the data stream on the python response realtime database feed
+let user_read = firebase.database().ref('users/' + username + '/read');
+
+user_read.on('value', function(snapshot) {
+  console.log(snapshot.val());
+});
