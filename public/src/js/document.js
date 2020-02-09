1f7ef10b53e4e5785d1208b7a0aa5436d89b5d79
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-02-09 11:45:11 -0500

    Updated Message Parse Board

--- public/src/js/document.js
+++ public/src/js/document.js
@@ -1,8 +1,20 @@
 
+// The global firebase database object
+let db = firebase.database();
+
+// Get the currently singed in user and the username
+let cUser;
+let username;
+
+// Dom tags for reference
+let suggestions = document.getElementById("suggestions");
+
 // Attach an onclick evetn listener arrow function for firebase signout (via logout button)
 document.getElementById('logout').onclick = () => {
 
-  //
+  // Wipe the database before logging out
+  firebase.database().ref('users/' + username + '/read').remove();
+
   firebase.auth().signOut().then(function() {
     // Sign-out successful, redirect back to the main page (index.html)
     window.location = "index.html";
@@ -12,13 +24,6 @@ document.getElementById('logout').onclick = () => {
   });
 
 }
-
-// The global firebase database object
-let db = firebase.database();
-
-// Get the currently singed in user and the username
-let cUser;
-let username;
 
 // Event Listener for DOM user update. Update intro text and update user related variables
 firebase.auth().onAuthStateChanged(function(user) {
@@ -33,10 +38,32 @@ firebase.auth().onAuthStateChanged(function(user) {
   // Update the user display info
   updateUserDisplay();
 
+  // Attach a listner for the data stream on the python response realtime database feed
+  let user_read = firebase.database().ref('users/' + username + '/read');
+
+  user_read.on('value', function(snapshot) {
+
+    // Get the ML generated question
+    let question = snapshot.val();
+
+    if (question === null) {
+    } else {
+      // Stringify the json snapshot value and parse it to get the message
+      let q_string = question.message;
+
+      console.log(question);
+
+      // Append the question to the suggestions div
+      suggestions.innerText = q_string;
+      suggestions.style.visibility = "visible";
+    }
+
+  });
+
 });
 
 // Database tester code
-document.getElementById('user-info').onclick = () => {
+document.getElementById('ask').onclick = () => {
 
   // Retreive the current user reference in the realtime database
   let user_dbRef = db.ref().child("users").child(username).child("post");
@@ -55,13 +82,6 @@ document.getElementById('user-info').onclick = () => {
   });
 
 }
-
-// Attach a listner for the data stream on the python response realtime database feed
-let user_read = firebase.database().ref('users/' + username + '/read');
-
-user_read.on('value', function(snapshot) {
-  console.log(snapshot.val());
-});
 
 // Update the document.html user display name info field
 function updateUserDisplay () {
